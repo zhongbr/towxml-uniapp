af7e2718b5eb45b705b252333251bc06d5491b48
zhong_br <44153622+zhongbr@users.noreply.github.com>
2020-10-10 13:13:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+// App端和H5引入需要全局引入Decode组件
+// #ifdef APP-PLUS || H5
+import Decode from './components/towxml/decode.vue';
+Vue.component('decode', Decode);
+// #endif
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
